19f73b767b7131c1a24ddb61150626fd3f8e03b8
Sunbeam <Sunbeam@Sunbeam.bcas.net>
2013-07-09 15:12:23 +0600

    ALA VAI

---
+++ Alauddin/src/ALA_Class.java
@@ -0,0 +1,12 @@
+
+public class ALA_Class {
+
+	public ALA_Class() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Im Bakul");
+	}
+
+}
